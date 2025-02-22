ff6544874e7ab4669cacf7c5828a212f79d704b6
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 22:57:02 -0300

    feat(Terrenos): adiciona enums `Clima` e `ExposicaoSolar`

---
+++ src/dominio/terrenos/enums/clima.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Terrenos::Enums
+{
+
+enum Clima
+{
+    TROPICAL,
+    TEMPERADO,
+    ARIDO,
+    EQUATORIAL,
+    SEMI_ARIDO,
+    SUBTROPICAL,
+    MEDITERRANEO,
+    POLAR,
+    MONTANHA
+};
+
+}
---
+++ src/dominio/terrenos/enums/exposicao_solar.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Terrenos::Enums
+{
+
+enum ExposicaoSolar
+{
+    SOL_PLENO,
+    MEIA_SOMBRA,
+    SOMBRA
+};
+
+}
