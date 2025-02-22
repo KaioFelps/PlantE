1ccf9f6c8e907799ea8a0152e6fb50353aec64c4
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 22:31:10 -0300

    change(Terreno): guarda um `unique_ptr` opcional, ao invés de um ponteiro opcional diretamente

--- src/dominio/terrenos/entidades/terreno.cpp
+++ src/dominio/terrenos/entidades/terreno.cpp
@@ -3,19 +3,9 @@
 namespace Terrenos::Entidades
 {
 
-Terreno::Terreno(unsigned int largura, unsigned int comprimento)
+Terreno::Terreno(unsigned int largura_, unsigned int comprimento_)
+    : largura(largura_), comprimento(comprimento_)
 {
-    this->largura = largura;
-    this->comprimento = comprimento;
-    this->solo = nullopt;
-}
-
-Terreno::~Terreno()
-{
-    if (this->solo.has_value())
-    {
-        delete this->solo.value();
-    }
 }
 
 unsigned int Terreno::obtenhaLargura() const
@@ -33,14 +23,14 @@ unsigned int Terreno::obtenhaTamanho() const
     return this->largura * this->comprimento;
 }
 
-optional<Solo*> Terreno::obtenhaSolo() const
+const optional<unique_ptr<Solo>>* Terreno::obtenhaSolo() const
 {
-    return this->solo;
+    return &this->solo;
 }
 
-void Terreno::atualizeSolo(Solo* solo)
+void Terreno::atualizeSolo(unique_ptr<Solo> solo)
 {
-    this->solo = make_optional(solo);
+    this->solo = std::move(solo);
 }
 
 } // namespace Terrenos::Entidades
