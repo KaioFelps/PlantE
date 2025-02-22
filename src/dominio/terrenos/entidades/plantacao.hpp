692469a853e0bff767effde8e44ffd6e8ca6f47a
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 21:48:45 -0300

    feat: adiciona entidade Plantacao - Headers - Implementações

---
+++ src/dominio/terrenos/entidades/plantacao.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "./planta.hpp"
+#include <ctime>
+#include <memory>
+#include <optional>
+
+namespace Terrenos::Entidades
+{
+
+class Plantacao
+{
+  private:
+    time_t dataDeInicio;
+    std::optional<time_t> dataDeFinalizacao;
+    std::optional<time_t> dataDeDesistencia;
+    std::unique_ptr<Planta> planta;
+
+    static time_t obtenhaTempoAtual();
+
+  public:
+    Plantacao(Planta* planta);
+
+    const time_t* obtenhaDataDeInicio() const;
+    const std::optional<time_t>* obtenhaDataDeFinalizacao() const;
+    const std::optional<time_t>* obtenhaDataDeDesistencia() const;
+    const Planta* obtenhaPlanta() const;
+
+    void finalize();
+    void cancele();
+};
+
+} // namespace Terrenos::Entidades
