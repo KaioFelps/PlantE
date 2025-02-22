ee90c438ec28ce30f432bd2b0016cc5db05ca226
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 21:48:45 -0300

    feat: adiciona entidade Plantacao - Headers - Implementações

---
+++ src/dominio/terrenos/entidades/plantacao.cpp
@@ -0,0 +1,52 @@
+#include "./plantacao.hpp"
+#include <chrono>
+#include <memory>
+
+using namespace std;
+
+namespace Terrenos::Entidades
+{
+
+Plantacao::Plantacao(Planta* planta_)
+    : planta(planta_), dataDeInicio(Plantacao::obtenhaTempoAtual())
+{
+}
+
+time_t Plantacao::obtenhaTempoAtual()
+{
+    auto now = chrono::system_clock::now();
+    auto nowTimeT = chrono::system_clock::to_time_t(now);
+    return nowTimeT;
+}
+
+const time_t* Plantacao::obtenhaDataDeInicio() const
+{
+    return &this->dataDeInicio;
+}
+
+const std::optional<time_t>* Plantacao::obtenhaDataDeFinalizacao() const
+{
+    return &this->dataDeFinalizacao;
+}
+
+const std::optional<time_t>* Plantacao::obtenhaDataDeDesistencia() const
+{
+    return &this->dataDeDesistencia;
+}
+
+const Planta* Plantacao::obtenhaPlanta() const
+{
+    return this->planta.get();
+}
+
+void Plantacao::cancele()
+{
+    this->dataDeDesistencia = Plantacao::obtenhaTempoAtual();
+}
+
+void Plantacao::finalize()
+{
+    this->dataDeFinalizacao = Plantacao::obtenhaTempoAtual();
+}
+
+} // namespace Terrenos::Entidades
