9d1c875e28688f7a1303e0eb6e07ba1eb137760e
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-22 19:52:32 -0300

    feat(enums): implementa o operador `<<` para todos os enums (para poderem ser impressos na tela)

--- src/dominio/terrenos/enums/exposicao_solar.hpp
+++ src/dominio/terrenos/enums/exposicao_solar.hpp
@@ -10,4 +10,27 @@ enum class ExposicaoSolar
     SOMBRA
 };
 
+// o modificador inline permite que a implementação seja feita diretamente no
+// cabeçalho, no entanto, o inline força o linker a deduplicar a definição dessa
+// função (mesmo sendo incluída em varios arquivos, no final, haverá somente uma
+// definição desta função)
+inline std::ostream& operator<<(std::ostream& IODevice,
+                                const ExposicaoSolar& exposicaosolar)
+{
+    switch (exposicaosolar)
+    {
+    case ExposicaoSolar::SOL_PLENO:
+        IODevice << "Sol pleno";
+        break;
+    case ExposicaoSolar::MEIA_SOMBRA:
+        IODevice << "Meia sombra";
+        break;
+    case ExposicaoSolar::SOMBRA:
+        IODevice << "Sombra";
+        break;
+    }
+
+    return IODevice;
 }
+
+} // namespace Terrenos::Enums
