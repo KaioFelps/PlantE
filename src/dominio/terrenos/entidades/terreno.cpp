55207bf56b152daddf92b81c656f4229772771a8
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 22:57:19 -0300

    feat(Terreno): adiciona propriedades `clima` e `exposicaoSolar`

--- src/dominio/terrenos/entidades/terreno.cpp
+++ src/dominio/terrenos/entidades/terreno.cpp
@@ -3,8 +3,12 @@
 namespace Terrenos::Entidades
 {
 
-Terreno::Terreno(unsigned int largura_, unsigned int comprimento_)
-    : largura(largura_), comprimento(comprimento_)
+Terreno::Terreno(unsigned int largura_,
+                 unsigned int comprimento_,
+                 Terrenos::Enums::ExposicaoSolar exposicaoSolar_,
+                 Terrenos::Enums::Clima clima_)
+    : largura(largura_), comprimento(comprimento_),
+      exposicaoSolar(exposicaoSolar_), clima(clima_)
 {
 }
 
@@ -28,9 +32,30 @@ const std::optional<std::unique_ptr<Solo>>* Terreno::obtenhaSolo() const
     return &this->solo;
 }
 
+Terrenos::Enums::ExposicaoSolar Terreno::obtenhaExposicaoSolar() const
+{
+    return this->exposicaoSolar;
+}
+
+Terrenos::Enums::Clima Terreno::obtenhaClima() const
+{
+    return this->clima;
+}
+
 void Terreno::atualizeSolo(std::unique_ptr<Solo> solo)
 {
     this->solo = std::move(solo);
+}
+
+void Terreno::coloqueExposicaoSolar(
+    Terrenos::Enums::ExposicaoSolar exposicaoSolar)
+{
+    this->exposicaoSolar = exposicaoSolar;
+}
+
+void Terreno::coloqueClima(Terrenos::Enums::Clima clima)
+{
+    this->clima = clima;
 }
 
 } // namespace Terrenos::Entidades
