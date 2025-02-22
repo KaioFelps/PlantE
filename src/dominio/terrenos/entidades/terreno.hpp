93c64d2848aab4371ac3c6cb48f6c5ccc722197f
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-21 22:57:19 -0300

    feat(Terreno): adiciona propriedades `clima` e `exposicaoSolar`

--- src/dominio/terrenos/entidades/terreno.hpp
+++ src/dominio/terrenos/entidades/terreno.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../enums/clima.hpp"
+#include "../enums/exposicao_solar.hpp"
 #include "./solo.hpp"
 #include <memory>
 #include <optional>
@@ -13,15 +15,25 @@ class Terreno
     unsigned int largura;
     unsigned int comprimento;
     std::optional<std::unique_ptr<Solo>> solo;
+    Terrenos::Enums::ExposicaoSolar exposicaoSolar;
+    Terrenos::Enums::Clima clima;
 
   public:
-    Terreno(unsigned int largura, unsigned int comprimento);
+    Terreno(unsigned int largura,
+            unsigned int comprimento,
+            Terrenos::Enums::ExposicaoSolar exposicaoSolar,
+            Terrenos::Enums::Clima clima);
+
     unsigned int obtenhaLargura() const;
     unsigned int obtenhaComprimento() const;
     unsigned int obtenhaTamanho() const;
     const std::optional<std::unique_ptr<Solo>>* obtenhaSolo() const;
+    Terrenos::Enums::ExposicaoSolar obtenhaExposicaoSolar() const;
+    Terrenos::Enums::Clima obtenhaClima() const;
 
     void atualizeSolo(std::unique_ptr<Solo> solo);
+    void coloqueExposicaoSolar(Terrenos::Enums::ExposicaoSolar exposicaoSolar);
+    void coloqueClima(Terrenos::Enums::Clima clima);
 };
 
 } // namespace Terrenos::Entidades
