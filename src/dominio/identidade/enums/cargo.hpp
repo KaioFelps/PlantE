4054ce988fc502abe190371991b7b6288338ce80
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-22 19:52:32 -0300

    feat(enums): implementa o operador `<<` para todos os enums (para poderem ser impressos na tela)

--- src/dominio/identidade/enums/cargo.hpp
+++ src/dominio/identidade/enums/cargo.hpp
@@ -1,3 +1,7 @@
+#pragma once
+
+#include <iostream>
+
 namespace Identidade::Enums
 {
 
@@ -8,4 +12,26 @@ enum class Cargo
     ADMINISTRADOR,
 };
 
+// o modificador inline permite que a implementação seja feita diretamente no
+// cabeçalho, no entanto, o inline força o linker a deduplicar a definição dessa
+// função (mesmo sendo incluída em varios arquivos, no final, haverá somente uma
+// definição desta função)
+inline std::ostream& operator<<(std::ostream& IODevice, const Cargo& cargo)
+{
+    switch (cargo)
+    {
+    case Cargo::ADMINISTRADOR:
+        IODevice << "Administrador";
+        break;
+    case Cargo::MODERADOR:
+        IODevice << "Moderador";
+        break;
+    case Cargo::USUARIO:
+        IODevice << "Usuário";
+        break;
+    }
+
+    return IODevice;
 }
+
+} // namespace Identidade::Enums
