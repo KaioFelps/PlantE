ef2b6530534a56e0cd40c06f1e156f5c6f4dc99c
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-24 15:14:13 -0300

    feat(Moderacao): adiciona enum `TipoDoDenunciavel`

---
+++ src/dominio/moderacao/enum/tipo_do_denunciavel.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+namespace Moderacao::Enums
+{
+
+enum class TipoDoDenunciavel
+{
+    USUARIO,
+    POSTAGEM,
+    COMENTARIO,
+};
+
+// o modificador inline permite que a implementação seja feita diretamente no
+// cabeçalho, no entanto, o inline força o linker a deduplicar a definição dessa
+// função (mesmo sendo incluída em varios arquivos, no final, haverá somente uma
+// definição desta função)
+inline std::ostream& operator<<(std::ostream& IODevice,
+                                const TipoDoDenunciavel& tipo)
+{
+    switch (tipo)
+    {
+    case TipoDoDenunciavel::USUARIO:
+        IODevice << "Usuário";
+        break;
+    case TipoDoDenunciavel::POSTAGEM:
+        IODevice << "Postagem";
+        break;
+    case TipoDoDenunciavel::COMENTARIO:
+        IODevice << "Comentário";
+        break;
+    }
+
+    return IODevice;
+}
+
+} // namespace Moderacao::Enums
