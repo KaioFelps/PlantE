38b04ab6bd87bfa9def1bc37b8f737811df0a00a
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-24 15:28:34 -0300

    feat(Denunciavel): implementa construtor base

---
+++ src/dominio/moderacao/entidades/denunciavel.cpp
@@ -0,0 +1,15 @@
+#include "denunciavel.hpp"
+#include "dominio/moderacao/enum/tipo_do_denunciavel.hpp"
+#include <boost/uuid.hpp>
+
+namespace Moderacao::Entidades
+{
+
+Denunciavel::Denunciavel(Enums::TipoDoDenunciavel tipo_) : tipo(tipo_)
+{
+    boost::uuids::random_generator uuidGenerator;
+    std::string strUuid = boost::uuids::to_string(uuidGenerator());
+    this->id = std::move(strUuid);
+}
+
+} // namespace Moderacao::Entidades
