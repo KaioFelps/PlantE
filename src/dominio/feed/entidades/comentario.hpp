843b0f305cb3d4ce8150c3a0cc834e65343075a9
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-24 19:24:27 -0300

    feat(Comentario): adiciona propriedades faltantes - autor; - postagem.

--- src/dominio/feed/entidades/comentario.hpp
+++ src/dominio/feed/entidades/comentario.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "comentavel.hpp"
+#include "dominio/identidade/entidades/usuario.hpp"
+#include "postagem.hpp"
+#include <memory>
 #include <string>
 
 namespace Feed::Entidades
@@ -8,8 +11,17 @@ namespace Feed::Entidades
 
 class Comentario : public Comentavel
 {
+  private:
+    std::shared_ptr<Identidade::Entidades::Usuario> autor;
+    std::shared_ptr<Postagem> postagem;
+
   public:
-    Comentario(std::string conteudo);
+    Comentario(std::shared_ptr<Identidade::Entidades::Usuario> autor,
+               std::shared_ptr<Postagem> postagem,
+               std::string conteudo);
+
+    const Postagem& obtenhaPostagem() const;
+    const Identidade::Entidades::Usuario& obtenhaAutor() const;
 };
 
 } // namespace Feed::Entidades
