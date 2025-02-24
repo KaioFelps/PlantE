93e98cf06fe5d412c7d5c9c46f0e70e432311ab2
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-24 18:53:56 -0300

    feat(Feed): adiciona entidade base `Comentavel`

---
+++ src/dominio/feed/entidades/comentavel.hpp
@@ -0,0 +1,31 @@
+#include "dominio/moderacao/entidades/denunciavel.hpp"
+#include <optional>
+#include <string>
+
+namespace Feed::Entidades
+{
+
+class Comentavel : public Moderacao::Entidades::Denunciavel
+{
+  protected:
+    Comentavel(std::string conteudo, Moderacao::Enums::TipoDoDenunciavel tipo);
+
+    std::string conteudo;
+    time_t dataPublicacao;
+    std::optional<time_t> dataRemocao;
+    bool estaAtivo;
+
+  private:
+    static time_t obtenhaDataAtual();
+
+  public:
+    const std::string& obtenhaConteudo() const;
+    time_t obtenhaDataPublicacao() const;
+    bool obtenhaEstaAtivo() const;
+    const std::optional<time_t>& obtenhaDataRemocao() const;
+
+    void desativar();
+    void coloqueConteudo(std::string conteudo);
+};
+
+} // namespace Feed::Entidades
