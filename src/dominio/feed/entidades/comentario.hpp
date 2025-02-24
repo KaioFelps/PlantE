351b5b4bde064f2ea2bfe3e08e9e65a363081ccc
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-24 19:12:04 -0300

    fix(Comentario): adiciona o método construtor

--- src/dominio/feed/entidades/comentario.hpp
+++ src/dominio/feed/entidades/comentario.hpp
@@ -1,10 +1,13 @@
 #include "comentavel.hpp"
+#include <string>
 
 namespace Feed::Entidades
 {
 
 class Comentario : public Comentavel
 {
+  public:
+    Comentario(std::string conteudo);
 };
 
 } // namespace Feed::Entidades
