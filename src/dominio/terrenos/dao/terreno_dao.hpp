967bb4093daa4b79724a2ba4d6b05abdf3d52315
JoaoR090 <jrzr432@gmail.com>
2025-02-26 17:59:21 -0300

    feito terreno_dao

--- src/dominio/terrenos/dao/terreno_dao.hpp
+++ src/dominio/terrenos/dao/terreno_dao.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "dominio/terrenos/entidades/terreno.hpp"
+#include "../entidades/terreno.hpp"
+#include "../../identidade/entidades/usuario.hpp"
+
 
 #include <memory>
 #include <optional>
@@ -12,8 +14,24 @@ namespace Terrenos::Dao
 class TerrenoDao
 {
   public:
-    virtual std::optional<std::shared_ptr<Entidades::Terreno>>
-    encontre(const std::string& idPlanta) = 0;
+    virtual std::optional<std::shared_ptr<Terrenos::Entidades::Terreno>>
+    encontre(const std::string& idTerreno) = 0;
+
+    virtual std::optional<std::shared_ptr<Terrenos::Entidades::Solo>>
+    choraSolo(const std::string& idTerreno,
+      float acidez,
+      float índiceDeMinerais,
+      float índiceDeSalinidade,
+      float índiceDeArgila,
+      float índiceDeSilte,
+      float cargaElétrica
+    ) = 0;
+
+    virtual std::optional<std::shared_ptr<Terrenos::Entidades::Terreno>>
+    salve(Terreno terreno) = 0;
+
+    virtual std::optional<std::shared_ptr<Identidade::Entidades::Usuario>>
+    listeTodosDoUsuário(Usuario usuario) = 0;
 };
 
 } // namespace Terrenos::Dao
