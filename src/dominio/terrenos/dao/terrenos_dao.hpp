61d0db4d9d9925da63d9c80613a605c462f76101
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-27 14:46:25 -0300

    refactor(TerrenosDao): corrige as assinaturas dos métodos

---
+++ src/dominio/terrenos/dao/terrenos_dao.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../../identidade/entidades/usuario.hpp"
+#include "../entidades/terreno.hpp"
+#include <memory>
+#include <optional>
+#include <vector>
+
+namespace Terrenos::Dao
+{
+
+class TerrenosDao
+{
+  public:
+    virtual std::optional<std::shared_ptr<Entidades::Terreno>>
+    encontre(const std::string& idTerreno) const = 0;
+
+    virtual std::shared_ptr<Entidades::Solo>
+    crieSolo(const std::string& idTerreno,
+             double acidez,
+             unsigned int índiceDeMinerais,
+             unsigned int índiceDeSalinidade,
+             unsigned int índiceDeArgila,
+             unsigned int índiceDeSilte,
+             double cargaElétrica) = 0;
+
+    virtual void salve(Entidades::Terreno terreno) = 0;
+
+    virtual std::vector<std::shared_ptr<Entidades::Terreno>>
+    listeTodosDoUsuario(Identidade::Entidades::Usuario usuario) const = 0;
+};
+
+} // namespace Terrenos::Dao
