e57fbcbc99f4724b6cdcb35f8c1e8379826488a8
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-27 18:40:59 -0300

    feat(GerenteDeTerrenos): implementa `listeTerrenos`

--- src/dominio/terrenos/gerentes/gerente_de_terrenos.cpp
+++ src/dominio/terrenos/gerentes/gerente_de_terrenos.cpp
@@ -1,4 +1,5 @@
 #include "gerente_de_terrenos.hpp"
+#include "dominio/identidade/dao/usuarios_dao.hpp"
 #include "dominio/terrenos/dao/plantas_dao.hpp"
 #include "dominio/terrenos/dao/terrenos_dao.hpp"
 #include <algorithm>
@@ -48,6 +49,25 @@ GerenteDeTerrenos::obtenhaSugestoes(const std::string& idTerreno) const
 std::vector<Entidades::Terreno>
 GerenteDeTerrenos::listeTerrenos(const std::string& idUsuario) const
 {
+    using Identidade::Dao::UsuariosDao;
+    using Identidade::Entidades::Usuario;
+    using Terrenos::Dao::TerrenosDao;
+
+    auto terrenosDao = this->contexto->obtenha<TerrenosDao>();
+    auto usuariosDao = this->contexto->obtenha<UsuariosDao>();
+
+    auto usuarioEncontrado = usuariosDao->encontre(idUsuario);
+
+    if (!usuarioEncontrado)
+    {
+        throw std::invalid_argument("Nâo existe um usuário com id \"" +
+                                    idUsuario + "\".");
+    }
+
+    auto usuario = std::dynamic_pointer_cast<Usuario>(*usuarioEncontrado);
+    auto terrenos = terrenosDao->listeTodosDoUsuario(*usuario);
+
+    return terrenos;
 }
 
 void GerenteDeTerrenos::recebaDadosDoSolo(std::string idTerreno,
