1136956acc3fd2f65b6081c43c3e3da4de410ddd
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-26 22:09:23 -0300

    feat(TipoDoDenunciavel): adiciona método para converter o enum para string

--- src/dominio/moderacao/enum/tipo_do_denunciavel.hpp
+++ src/dominio/moderacao/enum/tipo_do_denunciavel.hpp
@@ -16,22 +16,30 @@ enum class TipoDoDenunciavel
 // cabeçalho, no entanto, o inline força o linker a deduplicar a definição dessa
 // função (mesmo sendo incluída em varios arquivos, no final, haverá somente uma
 // definição desta função)
-inline std::ostream& operator<<(std::ostream& IODevice,
-                                const TipoDoDenunciavel& tipo)
+inline std::string tipoDoDenunciavelParaString(TipoDoDenunciavel tipo)
 {
     switch (tipo)
     {
     case TipoDoDenunciavel::USUARIO:
-        IODevice << "Usuário";
-        break;
+        return "Usuário";
     case TipoDoDenunciavel::POSTAGEM:
-        IODevice << "Postagem";
-        break;
+        return "Postagem";
     case TipoDoDenunciavel::COMENTARIO:
-        IODevice << "Comentário";
-        break;
+        return "Comentário";
+    default:
+        throw std::runtime_error(
+            "Tentativa de stringificar um TipoDoDenunciavel inválido.");
     }
+}
 
+// o modificador inline permite que a implementação seja feita diretamente no
+// cabeçalho, no entanto, o inline força o linker a deduplicar a definição dessa
+// função (mesmo sendo incluída em varios arquivos, no final, haverá somente uma
+// definição desta função)
+inline std::ostream& operator<<(std::ostream& IODevice,
+                                const TipoDoDenunciavel& tipo)
+{
+    IODevice << tipoDoDenunciavelParaString(tipo);
     return IODevice;
 }
 
