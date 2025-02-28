f25fef8e7f52826cb2cf625dadab6c12ba3fab96
Kaio Felps <kaiofelipe2005@gmail.com>
2025-02-27 22:40:34 -0300

    change(PlantasDao): retorna um vetor compartilhado ao invés de uma cópia direta de `Planta` no método `encontre`

--- src/dominio/terrenos/dao/plantas_dao.hpp
+++ src/dominio/terrenos/dao/plantas_dao.hpp
@@ -15,7 +15,7 @@ class PlantasDao
     virtual std::vector<Entidades::Planta>
     encontrePlantasCorrespondentes(const Entidades::Solo& solo) = 0;
 
-    virtual std::optional<Entidades::Planta>
+    virtual std::optional<std::shared_ptr<Entidades::Planta>>
     encontre(const std::string& idPlanta) = 0;
 
     virtual std::vector<Entidades::Planta> liste() const = 0;
